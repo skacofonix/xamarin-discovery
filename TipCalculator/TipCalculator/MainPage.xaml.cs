2b708221e9b1ac01b8ab5871a489444a946b3197
Yannick <yannick-externe.valy@enedis.fr>
2018-11-07 23:32:19 +0100

    Add TipsCalculator

---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace TipCalculator
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            AmountEntry.TextChanged += (sender, args) => ComputeTips();
+            TipsPercentSlider.ValueChanged += (sender, args) =>
+            {
+                var percent = Math.Round(args.NewValue * 100, 0);
+                TipsPercentLabel.Text = $"Tips {percent:#0}%";
+
+                ComputeTips();
+            };
+
+            ComputeTips();
+        }
+
+        private void ComputeTips()
+        {
+            if (double.TryParse(AmountEntry.Text, out var amount))
+            {
+                var tips = amount * TipsPercentSlider.Value;
+                TipsLabel.Text = tips.ToString(CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                TipsLabel.Text = "N/A";
+            }
+        }
+    }
+}
