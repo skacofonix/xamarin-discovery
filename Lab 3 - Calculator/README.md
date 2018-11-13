6833d3ab27ffd9883d2081c1ce27168b9d19aa53
Yannick <yannick-externe.valy@enedis.fr>
2018-11-13 12:43:14 +0100

    Lab 3 - Done!

---
+++ Lab 3 - Calculator/README.md
@@ -0,0 +1,8 @@
+# Lab 3 - Calculator
+Créer une vue en XAML
+
+1. Définir la grille de base
+
+2. Câbler les bouttons
+
+3. Un peu de couleur sur les boutons "opérations"
---
+++ Lab 3 - Calculator/Start/Calculator/SharedResources.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace Calculator
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor => Color.FromRgb(0xff, 0xa5, 0);
+    }
+}
